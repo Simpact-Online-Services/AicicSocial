304b8cebea62df68837c426a5218e2e058f140b6
mdmonis <mdmonis128@gmail.com>
2024-03-29 12:38:31 +0530

    Update: AuthNavigator Added Forgot and Reset Screen

--- src/navigations/AuthNavigator.js
+++ src/navigations/AuthNavigator.js
@@ -4,6 +4,8 @@ import Login from '../screens/auth/LoginScreen';
 import React from 'react'
 import SignUp from '../screens/auth/SignUpScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ForgotPassword from '../screens/ForgotPassword';
+import ResetPassword from '../screens/ResetPassword';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function AuthNavigator() {
    <Stack.Navigator>
     <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
     <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
+    <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: false}}/>
+    <Stack.Screen name="ResetPassword" component={ResetPassword} options={{headerShown: false}}/>
    </Stack.Navigator>
   )
 }
